89d0c032d8d678cf6698900479a1172a3053ca7b
Amanbek Asylbekov <amanbek.asylbekov@tvh.com>
2023-03-05 18:03:15 +0600

    fix: payment

--- functions/v1Apis.js
+++ functions/v1Apis.js
@@ -4,70 +4,119 @@ const firebase = require("firebase-admin");
 const { getStorage } = require("firebase-admin/storage");
 const cors = require("cors");
 const express = require("express");
-const boot = require("../app.js")
-
+const axios = require('axios');
+const crypto = require('crypto')
+const FormData = require('form-data');
+const xml2js = require('xml2js');
+const { v4: uuidv4 } = require('uuid');
 
 // Let's start express for API
 const app = express();
 
 // Automatically allow cross-origin requests
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.options("*", cors());
 
 // The var represents firestore db
 let db = firebase.firestore();
 
-const stripe = require("stripe")(
-  "sk_test_51LECBfDaK9yndacRLez0GLV3GQrFzUwR8HWxnqW0cVJfPfrEaYwTG5YCw4jTD1mNVa67zLBXKMbLWw1C1n4tl3JU00QfgLbL3U"
-);
 
 
-/** Create a payment link for
- * :product Any string as a product name
- * :amount in cents (like 200 for 2)
- * :currency like eur or gbp
- * Examples:
- * GET https://us-central1-nfttrx.cloudfunctions.net/v1/paymentlink/NFT%20purchase/10000/gbp
- * POST https://us-central1-nfttrx.cloudfunctions.net/v1/paymentlink/
- *  with args 	"product":"Idea NFT ID#123456"
- *		 		"amount":"200000"
- *		 		"currency":"eur"
- * Returns:
- * Stripe's payment link object
- * https://stripe.com/docs/api/payment_links/payment_links/object
- * contains property "url"
- */
-//
-//
-app.all("/paymentlink/:product/:amount/:currency", async (req, res) => {
-  let productName = req.params.product;
-  let amount = +req.params.amount;
-  let currency = req.params.currency;
-  const stripeProduct = await stripe.products.create({
-    name: productName,
-  });
-  const price = await stripe.prices.create({
-    unit_amount: amount,
-    currency: currency,
-    product: stripeProduct.id,
+async function boot(additional) {
+  console.log('init payment');
+
+  const { amount, description, ...other } = additional;
+
+  const env = {
+    paybox_url: 'https://api.freedompay.money/init_payment.php', // Базовый url для API(По умолчанию https://api.freedompay.money)
+    paybox_merchant_id: '548469', // ID магазина на стороне FreedomPay
+    paybox_merchant_secret: 'cJXMfnuxLWnF4MnJ', // Секретный ключ(для приема платежей) магазина на стороне FreedomPay
+    result_url: 'https://webhook.site/e24d78a4-8415-449f-9977-4775084ff613', // result_url
+  }
+  
+  const initPaymentData = {
+    pg_order_id: uuidv4(), // Идентификатор платежа в системе мерчанта. Рекомендуется поддерживать уникальность этого поля.
+    pg_merchant_id: env.paybox_merchant_id, // Идентификатор мерчанта в FreedomPay Выдается при подключении.
+    pg_amount: amount, // Сумма платежа в валюте pg_currency.
+    pg_description: description, // Описание товара или услуги. Отображается покупателю в процессе платежа.
+    pg_salt: 'some random string',
+    pg_result_url: env.result_url,
+    ...other
+  }
+
+  function sortObjectKeysAlphabetically(obj) {
+    const keys = Object.keys(obj).sort();
+    const sortedObj = {};
+    
+    for (let key of keys) {
+      sortedObj[key] = obj[key];
+    }
+    
+    return sortedObj;
+  }
+  
+  
+  const sortedObj = sortObjectKeysAlphabetically(initPaymentData);
+  const convertedToArr = Object.values(sortedObj);
+  
+  convertedToArr.unshift('init_payment.php');
+  convertedToArr.push(env.paybox_merchant_secret);
+  
+  initPaymentData.pg_sig = crypto.createHash('md5').update(convertedToArr.join(';')).digest("hex")
+  
+  const formData = new FormData();
+  
+  for (const key in initPaymentData) {
+    formData.append(key, initPaymentData[key]);
+  }
+
+  const result = await axios.post(env.paybox_url, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
   });
 
-  const paymentLink = await stripe.paymentLinks.create({
-    line_items: [
-      {
-        price: price.id,
-        quantity: 1,
-      },
-    ],
+  console.log(initPaymentData)
+
+  let resultObj;
+
+  xml2js.parseString(result.data, function(err, result) {
+      if (err) {
+        resultObj = err;
+      } else {
+        resultObj = result
+      }
   });
-  res.send(paymentLink);
-});
+
+  return resultObj;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.post("/paymentinit", async (req, res) => {
-
   const additional = {
     tourStartDate: req.body.tourStartDate,
     places: req.body.places,
@@ -77,44 +126,49 @@ app.post("/paymentinit", async (req, res) => {
     transportId: req.body.transportId,
     userId: req.body.userId,
     amount: req.body.amount,
-    description: req.body.description
-  }
+    description: req.body.description,
+  };
 
   const result = await boot(additional);
-
+  console.log("RESULT=========57========>",result);
   res.json(result);
 });
 
-
 // На этот ендпойт будет приходить запрос от paybox. Это наш вебхук. Пример https://localhost:3000/webhook/payment-result
 // Мы здесь получаем данные в req.body. Все нужные данные я уже достал тут. Нужно теперь просто записать их в firbase.
-// Когда задеплойим, от vercel мы получим домен https://{domen}/webhook/payment-result. 
-// Этот домен нужно дать мне. Я его пропишу в настройки paybox. 
-app.post("/webhook/payment-result", async (req, res) => { 
-
-  const { 
-    pg_order_id, 
-    pg_payment_id, 
-    pg_amount, 
-    pg_currency, 
-    pg_description, 
+// Когда задеплойим, от vercel мы получим домен https://{domen}/webhook/payment-result.
+// Этот домен нужно дать мне. Я его пропишу в настройки paybox.
+app.post("/webhook/payment-result", async (req, res) => {
+  const {
+    pg_order_id,
+    pg_payment_id,
+    pg_amount,
+    pg_currency,
+    pg_description,
     tourStartDate,
     places,
     schedule,
     time,
     tourId,
     transportId,
-    userId
-  } = req.body
+    userId,
+  } = req.body;
 
-  res.json(200);
-
+  res.json({
+    pg_order_id,
+    pg_payment_id,
+    pg_amount,
+    pg_currency,
+    pg_description,
+    tourStartDate,
+    places,
+    schedule,
+    time,
+    tourId,
+    transportId,
+    userId,
+  });
 });
-
-
-app.get("/", async (req, res) => {
-  res.json('hello pidaras');
-})
 
 /** Create a payment link for
  * :product Any string as a product name
@@ -202,7 +256,7 @@ app.get("/orders", async (req, res) => {
         .where("miniStops", "array-contains", tour.to)
         .get();
       expressData.forEach((doc) => {
-        const s = doc.data().stops.find((el) => el.id === tour.to.id)
+        const s = doc.data().stops.find((el) => el.id === tour.to.id);
         half_expressData.push({
           transportId: doc.id,
           ...doc.data(),
@@ -234,8 +288,8 @@ app.get("/orders", async (req, res) => {
       if (half_expressData.length) {
         if (!data.length) {
           data = half_expressData;
-        } else if(data.length) {
-          data = [...data, ...half_expressData]
+        } else if (data.length) {
+          data = [...data, ...half_expressData];
         }
       }
 
@@ -261,7 +315,9 @@ app.get("/orders", async (req, res) => {
           }
           return { ...item, placesCount: 17, orderExist: false };
         });
-        const result = newTransports.filter((el) => Number(el.placesCount) >= Number(q.places) )
+        const result = newTransports.filter(
+          (el) => Number(el.placesCount) >= Number(q.places)
+        );
         res.json(result);
       } else {
         res.json(
